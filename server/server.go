ee68b352d69e6003d7f7ba9ba0909a05d2f35263
Shelley <shelley@exe.dev>
2026-03-11 23:52:52 +0000

    slack: make onAgentDone async to avoid blocking SSE broadcasts
    Co-authored-by: Shelley

--- server/server.go
+++ server/server.go
@@ -1063,7 +1063,7 @@ func (s *Server) publishConversationState(state ConversationState) {
 		notifEvent = &event
 
 		if s.onAgentDone != nil {
-			s.onAgentDone(state.ConversationID)
+			go s.onAgentDone(state.ConversationID)
 		}
 	}
 
